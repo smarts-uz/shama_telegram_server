f94a6aa42a08bda675a1445ecc1246f4336cfdb5
tajbaevshamsiddin <100567773+tajbaevshamsiddin@users.noreply.github.com>
2023-10-08 21:00:36 +0500

    add compose file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from aiogram.types import Message, Document
 from aiogram.utils.markdown import hbold
 
 TOKEN = getenv('6321096156:AAGg2IoFSMNmaMwbE4V5-GkxgSbR063Oh0s')
-BASE_URL = 'http://127.0.0.1:8081/bot'
+BASE_URL = 'http://127.0.0.1:8083/bot'
 bot = Bot(token='6321096156:AAGg2IoFSMNmaMwbE4V5-GkxgSbR063Oh0s')
 dp = Dispatcher(base_url=BASE_URL, token=TOKEN)
 
@@ -19,9 +19,9 @@ dp = Dispatcher(base_url=BASE_URL, token=TOKEN)
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
     sent_message = await bot.send_document(chat_id=message.chat.id, document='https://t.me/downloadsgame/3471')
+    print(message.chat.id)
     file_id = sent_message.document.file_id
     await bot.get_file(file_id)
-
 
 @dp.message()
 async def echo_handler(message: Document) -> None:
@@ -34,7 +34,7 @@ async def echo_handler(message: Document) -> None:
 async def main() -> None:
     bot = Bot(token='6321096156:AAGg2IoFSMNmaMwbE4V5-GkxgSbR063Oh0s', parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
-    # await bot.log_out()
+    await bot.log_out()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
