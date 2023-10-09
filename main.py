e800f478dd2f430568ab34646370786257f53b75
tajbaevshamsiddin <100567773+tajbaevshamsiddin@users.noreply.github.com>
2023-10-09 20:20:17 +0500

    finish

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ from telegram.ext import Updater
 
 
 TOKEN = getenv('6321096156:AAGg2IoFSMNmaMwbE4V5-GkxgSbR063Oh0s')
-BASE_URL = 'http://127.0.0.1:8082/bot'
+BASE_URL = 'http://127.0.0.1:8081/bot'
 bot = Bot(token='6321096156:AAGg2IoFSMNmaMwbE4V5-GkxgSbR063Oh0s')
 dp = Dispatcher(base_url=BASE_URL, token=TOKEN)
 api_id = 20448273
 api_hash = '25a0ad7791e66b9f53d79c52178ec358'
 
-
 with TelegramClient('anon', api_id, api_hash) as client:
+    client.start()
     @dp.message(CommandStart())
+
     async def command_start_handler(message: Message) -> None:
         await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-        sent_message = await bot.send_document(chat_id=message.chat.id, document='https://t.me/downloadsgame/3482')
+        sent_message = await bot.send_document(chat_id=message.chat.id, document='123.zip')
         print(message.chat.id)
         file_id = sent_message.document.file_id
         print(file_id)
@@ -42,7 +43,7 @@ with TelegramClient('anon', api_id, api_hash) as client:
     async def main() -> None:
         bot = Bot(token='6321096156:AAGg2IoFSMNmaMwbE4V5-GkxgSbR063Oh0s', parse_mode=ParseMode.HTML)
         await dp.start_polling(bot)
-        # await bot.log_out()
+        await bot.log_out()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
